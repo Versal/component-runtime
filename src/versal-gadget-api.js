0b6f499c2d6bedbede0b466fd416bfe531653e16
Andrey Mereskin <mereskin@gmail.com>
2014-03-26 08:45:11 -0600

    add few comments to gadget API

--- src/versal-gadget-api.js
+++ src/versal-gadget-api.js
@@ -1,19 +1,41 @@
-// Represents gadget for the player
+/*
+VersalGadget provides a wrapper over iframe.contentWindow so player
+can send updates to the gadget.
+
+{@recipient} - something, that supports postMessage
+  For the case of iframe, it is iframe.contentWindow.
+{@origin} - optional. Defaults to window.location.origin
+
+*/
+
 var VersalGadget = function(recipient, origin){
-  this.recipient = recipient;
-  this.origin = origin;
+  // iframe can be provided as a recipient
+  if(recipient && recipient.localName == 'iframe') {
+    recipient = recipient.contentWindow;
+  };
+
+  if(recipient && recipient.postMessage) {
+    this.recipient = recipient;
+  } else {
+    throw new Error('Gadget API: Provided recipient does not support postMessage');
+  };
+
+  this.origin = origin || window.location.origin;
 };
 
-var SUPPORTED_MESSAGES = ['setAttribute', 'attached', 'detached'];
+var KNOWN_MESSAGES = ['setAttribute', 'attached', 'detached'];
 
 VersalGadget.prototype = {
 
+  // Send all the attributes on initialization
+  // or when gadget is modified by a collaborator
   sendAttributes: function(attrs) {
     Object.getOwnPropertyNames(attrs).forEach(function(name){
       this.sendAttribute(name, attrs[name]);
     }.bind(this));
   },
 
+  // Send attribute updates when individual property sheet fields change
   sendAttribute: function(name, value) {
     if(name) {
       var attr = {};
@@ -22,18 +44,20 @@ VersalGadget.prototype = {
     }
   },
 
+  // Send arbitrary message. Name has to be in KNOWN_MESSAGES
   send: function(name, detail){
     if(this.isKnownMessage(name)) {
-      // We don't need to JSON.stringfy our messages.
-      // postMessage handles that for us.
+      // We don't need to JSON.stringfy our messages
+      // since postMessage handles that for us
       this.recipient.postMessage(this.createMessage(name, detail), this.origin);
     }
   },
 
   isKnownMessage: function(name){
-    return SUPPORTED_MESSAGES.indexOf(name) >= 0;
+    return KNOWN_MESSAGES.indexOf(name) >= 0;
   },
 
+  // Wrap name and detail in envelope
   createMessage: function(name, detail) {
     var message = { name: name };
     if(detail) {
