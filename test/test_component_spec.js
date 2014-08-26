ab9f66342a3e5047df4eb25a52cd7a4058b95194
Jan Paul <janpaul123@users.noreply.github.com>
2014-08-26 11:33:06 -0700

    Fix the order of HtmlImports/CustomElements and the event in the test
    Note that before this our tests failed in Chrome 36, as it doesn’t fire HTMLImportsLoaded anymore

--- test/test_component_spec.js
+++ test/test_component_spec.js
@@ -1,6 +1,6 @@
 describe('test-component', function() {
   it('renders NEW CONTENT instead of the original content', function(done){
-    window.addEventListener('HTMLImportsLoaded', function(e) {
+    window.addEventListener('WebComponentsReady', function(e) {
       var div = document.createElement('div');
       div.innerHTML = '<test-component data-new-content="NEW CONTENT">Original content</test-component>';
       document.body.appendChild(div);
