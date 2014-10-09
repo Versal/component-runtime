f906bf6a535be32e67cb9c6728bec47807a3fd51
Shaohua Zhou <shao@versal.com>
2014-10-09 13:58:23 -0700

    Add one more test for detach callback

--- test/test_component_spec.js
+++ test/test_component_spec.js
@@ -1,14 +1,47 @@
 describe('test-component', function() {
-  it('renders NEW CONTENT instead of the original content', function(done){
-    window.addEventListener('WebComponentsReady', function(e) {
-      var div = document.createElement('div');
-      div.innerHTML = '<test-component data-new-content="NEW CONTENT">Original content</test-component>';
-      document.body.appendChild(div);
-
-      setTimeout(function(){
-        chai.expect(div.children[0].innerHTML).to.eq('NEW CONTENT');
-        done();
-      }, 100);
+  //WebComponentsReady event is
+  //fired only once after custom elments polyfill finished its start up tasks
+  //all subequent upgrades are done by MutationObserver hence no more events
+  var promise = new Promise(function(resolve, reject){
+    window.addEventListener('WebComponentsReady', function() {
+      resolve();
     });
+  });
+
+  beforeEach(function(done) {
+    promise.then(function(){
+      done();
+    });
+  });
+
+  it('renders NEW CONTENT instead of the original content', function(done){
+    var div = document.createElement('div');
+    div.innerHTML = '<test-component data-new-content="NEW CONTENT">Original content</test-component>';
+    document.body.appendChild(div);
+
+    setTimeout(function(){
+      //need to wait for upgrade to finish
+      var element = document.querySelector('test-component');
+      chai.expect(element.innerHTML).to.eq('NEW CONTENT');
+      document.body.removeChild(div);
+      done();
+    }, 0);
+  });
+
+  it('fires detachedCallback', function(done){
+    var div = document.createElement('div');
+    div.innerHTML = '<test-component data-new-content="NEW CONTENT">Original content</test-component>';
+    document.body.appendChild(div);
+
+    setTimeout(function(){
+      //need to wait for upgrade to finish
+      var element = document.querySelector('test-component');
+      element.detachedCallback = function(){
+        chai.expect(true).to.eq(true);
+        done();
+      };
+      document.body.removeChild(div);
+    }, 0);
+
   });
 });
